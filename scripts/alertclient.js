c3e20ca099fe47e34f573d42664d7410b7810951
Sparta Cheung <sparta.cheung@navy.mil>
2014-07-09 13:19:16 -0700

    adding alert elements to accordion

--- scripts/alertclient.js
+++ scripts/alertclient.js
@@ -48,6 +48,8 @@ $(function start() {
 
    var user = 'icodeuser';
 
+   var receivedAlertRules = false;
+
    var alertCountTotal = 0;
 
    //if user is running Mozilla then use its built-in WebSocket
@@ -63,13 +65,14 @@ $(function start() {
 
    //Open socket connection to server
    var connection = new WebSocket('ws://128.49.78.214:2411');
-
+      //==================== Opened connection to the server =========================
       connection.onopen = function () {
          //Connected to server success
          alertLabel.html('<b>Connected to alert server as user: </b>' + user);
          connection.send(user);
       };
 
+      //==================== Closed connection to server =============================
       connection.onclose = function () {
          console.log('Server is down');
          alertLabel.text('Server is down');
@@ -80,7 +83,7 @@ $(function start() {
          }, 1000);
       };
 
-      // most important part - incoming messages
+      //==================== Incoming messages from the server =======================
       connection.onmessage = function (message) {
          // try to parse JSON message. Because we know that the server always returns
          // JSON this should work without any problem but we should make sure that
@@ -92,10 +95,38 @@ $(function start() {
             return;
          }
 
+         //---------------------- Server connection response received ----------------
          if (json.type === 'response') {
             var serverResponse = json.data;
             console.log('Alert server accepted the connection: ', serverResponse);
          }
+         //---------------------- Alert Rules received -------------------------------
+         else if (json.type === 'alertRules') {
+            //Received an alert that the server retrieved from the database
+            //if alerts have already been received (i.e. server restarted) then
+            // remove old alerts and add the new ones.
+            if (receivedAlertRules) {
+               //TODO: remove old alerts to add the new ones
+            }
+
+            //Set received alertRules to true now that we are receiving the alerts
+            receivedAlertRules = true;
+
+            //Create a new menu element content for the alert accordion element
+            var singleAlert = json.data;
+            console.log('Received alert:', singleAlert.alert_id);
+
+            var accordionElement = '<h3>Alert ' + singleAlert.alert_id + ' for ' + singleAlert.user_id + ' (<span id="alertCount-' + singleAlert.alert_id + '">0</span>)' + '</h3>';
+            accordionElement += '<div id="' + 'alert_id' + singleAlert.alert_id + '" class="alertElement">';
+            accordionElement += '</div>';
+
+            //Create new accordion element
+            $("#alertAccordion").append(accordionElement).accordion('destroy').accordion();
+
+            var elementContent = document.getElementById('alert_id' + singleAlert.alert_id);
+            elementContent.innerHTML += JSON.stringify(singleAlert, null, ' ');
+         }
+         //---------------------- Alert received -------------------------------------
          else if (json.type === 'alertNotification') {
             var decodedAIS = JSON.parse(json.data);
 
@@ -124,18 +155,27 @@ $(function start() {
                shapeFadeOut(alertVesselCircle, 2, null);
             }, 3000);
          }
-         else if (json.type === 'alertMatchingCriteria') {
+         //---------------------- Alert Rule matching Alert received -----------------
+         else if (json.type === 'alertMatched') {
             console.log(json.data);
+            //display on summary accordion element
             content.prepend(json.data + '<br>');
+
+            //increment count on alert element title
+            var alertCountSpan = document.getElementById('alertCount-'+json.data);
+            alertCountSpan.innerHTML = parseInt(alertCountSpan.innerHTML) + 1;
          }
+         //---------------------- Alert History --------------------------------------
          else if (json.type === 'alertHistory') {
          }
+         //---------------------- Progress received ----------------------------------
          else if (json.type === 'totalDecoded') {
             console.log('Alert server sent progress report');
             //content.prepend(json.data + '<br>');
             //toastr.info(json.data)
             processedCountLabel.html(json.data);
          }
+         //---------------------- Unknown message received ---------------------------
          else {
             console.log('Alert server sent unrecognized data', json);
          }
