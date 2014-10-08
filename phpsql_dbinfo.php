050306396289e37f25f3a237342bd3f93619db5c
Sparta Cheung <sparta.cheung@navy.mil>
2014-10-08 15:16:52 -0700

    adding php template file back

---
+++ phpsql_dbinfo.php
@@ -0,0 +1,16 @@
+<?php
+/* Connection data */
+$odbc_driver      = 'MySQL ODBC 5.3 Unicode Driver';
+$ais_database     = 'icodemda';
+//Switch between vessels_memory or vessels table
+$vessels_table    = 'vessels_memory';  //'vessels';
+
+$laisic_database  = 'laisic';
+$laisic_live_database  = 'icodemaps';
+$ihs_database     = 'wros';
+$port_database    = 'risk';
+$alert_database   = 'alerts';
+$odbc_host        = 'localhost';
+$odbc_user        = 'USERNAME';
+$odbc_password    = 'PASSWORD';
+?>
